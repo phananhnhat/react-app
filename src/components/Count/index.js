8eee538997ac8099ebc75ad3ed2c7d6afc040dbe
nhatpa <anhnhnhatphan1995@gmail.com>
2022-03-15 20:22:08 +0700

    comment add

--- src/components/Count/index.js
+++ src/components/Count/index.js
@@ -34,6 +34,11 @@ export default class Home extends React.Component {
   render() {
     // TODO: setState 1 lần nhưng vào render 2 lần
     // TODO: https://mariosfakiolas.com/blog/my-react-components-render-twice-and-drive-me-crazy/
+    // TODO: DO sử dụng StrictMode
+    // Your app is using StrictMode. See your index.js file - your app is wrapped between a <React.StrictMode> element.
+    // Using StrictMode will cause your app to render twice, but only in development mode. Creating an app with create-react-app will enable strict mode by default.
+    // https://reactjs.org/docs/strict-mode.html
+
     // console.log giữa 2 lần chạy render đã bị React ghi đè
     console.log('1111 render' + this.test, this.test + 'a');
     this.test = this.test +  3
