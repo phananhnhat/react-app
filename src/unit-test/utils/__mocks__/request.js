fcad069b9bec7b794ce5253359a4e53251b85908
nhatpa <anhnhnhatphan1995@gmail.com>
2023-01-31 15:43:38 +0700

    Roll back jest config

--- src/unit-test/utils/__mocks__/request.js
+++ src/unit-test/utils/__mocks__/request.js
@@ -3,6 +3,8 @@ const users = {
   5: {name: 'Paul'},
 };
 
+// Because we don't want to go to the network in our test, we are going to create a manual mock for our request.js module in the __mocks__ folder (the folder is case-sensitive, __MOCKS__ will not work).
+// It could look something like this:
 export default function request(url) {
   return new Promise((resolve, reject) => {
     const userID = parseInt(url.substr('/users/'.length), 10);
